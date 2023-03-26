a6e6dea3070234b55111ef6254b6f276a276c241
Ankrio <Ankrio@ANKRI-PC>
2023-03-26 15:17:26 +0300

    Add EF

---
+++ PocketSafe.DAL.EF/PostgreeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PocketSafe.Domain.Models;
+using T = PocketSafe.Domain.Models;
+
+namespace TaskProject.DAL.EF
+{
+    public class PostgreeContext : DbContext
+    {
+        public PostgreeContext(DbContextOptions<PostgreeContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<T.Task> Tasks => Set<T.Task>();
+    }
+}
